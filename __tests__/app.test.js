0a0a80cff0b81c482e373d0f260b67559af7daef
reganmatt <104975201+reganmatt@users.noreply.github.com>
2022-08-11 12:38:16 +0100

    complete 08 ticket with testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,7 +13,7 @@ describe("/students", () => {
         .get("/students")
         .expect(200)
         .then(({ body }) => {
-          console.log(body);
+          // console.log(body);
         });
     });
   });
@@ -26,7 +26,36 @@ describe("/:country", () => {
         .get("/englandDB")
         .expect(200)
         .then(({ body }) => {
-          console.log(body);
+          // console.log(body);
+        });
+    });
+  });
+});
+
+describe("/student/:username", () => {
+  describe("GET /student/:username", () => {
+    test("status 200: returns with an object of specified users info", () => {
+      return request(app)
+        .get("/student/mario")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.profile).toHaveLength(1);
+          expect(body.profile[0]).toMatchObject({
+            _id: "62f4e68fd27bf3981e8ee25e",
+            avatarId: "https://i.imgur.com/mM4LmUV.png",
+            email: "mario@gmail.com",
+            jeo_ranch: [
+              "https://i.imgur.com/oxYZ7c2.png",
+              "https://i.imgur.com/T5IjKoI.png",
+              "https://i.imgur.com/x0cwPla.png",
+              "https://i.imgur.com/mM4LmUV.png",
+            ],
+            password: "unique",
+            type: "student",
+            userPoints: 10,
+            userStatus: "I am a geography quiz whiz!",
+            username: "mario",
+          });
         });
     });
   });

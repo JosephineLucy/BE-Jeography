ba7072225156fe14bb7b71369e7e687b3b7c55bd
Rutely Burgos <rcbcastillo@gmail.com>
2022-08-11 12:06:19 +0100

    Add TDD to the /api/student post request endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,3 +31,24 @@ describe("/:country", () => {
     });
   });
 });
+
+describe('/student', () => {
+  describe('POST /student', () => {
+    test('status 201: responds with a body of a posted student', () => {
+      const studentToPost = {
+        username:'Jamie',
+        password:'letsHope123',
+        email: 'jamiete@gmail.com'
+      };
+
+      return request(app)
+      .post('/student')
+      .send(studentToPost)
+      .expect(201)
+      .then(({body}) => {
+        expect(body.student).toEqual({acknowledged: true, insertedId: expect.any(String)})
+      
+      });
+    });
+  });
+});

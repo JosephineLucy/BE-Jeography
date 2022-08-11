6bdd4ce7e54b5c9dea6d238a414d0d580212c66d
reganmatt <104975201+reganmatt@users.noreply.github.com>
2022-08-11 12:48:18 +0100

    correction to main syntax in test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,7 +13,6 @@ describe("/students", () => {
         .get("/students")
         .expect(200)
         .then(({ body }) => {
-
           body.students.forEach((student) => {
             expect.objectContaining({
               _id: expect.any(String),
@@ -39,7 +38,16 @@ describe("/:country", () => {
         .get("/englandDB")
         .expect(200)
         .then(({ body }) => {
-
+          expect(body.selectedCountry).toHaveLength(5);
+          body.selectedCountry.forEach((question) => {
+            expect.objectContaining({
+              _id: expect.any(String),
+              country: expect.any(String),
+              question: expect.any(String),
+              correct_answer: expect.any(String),
+              incorrect_answers: expect.any(Array),
+            });
+          });
         });
     });
   });
@@ -68,16 +76,6 @@ describe("/student/:username", () => {
             userPoints: 10,
             userStatus: "I am a geography quiz whiz!",
             username: "mario",
-          expect(body.selectedCountry).toHaveLength(5);
-          body.selectedCountry.forEach((question) => {
-            expect.objectContaining({
-              _id: expect.any(String),
-              country: expect.any(String),
-              question: expect.any(String),
-              correct_answer: expect.any(String),
-              incorrect_answers: expect.any(Array),
-            });
-
           });
         });
     });
